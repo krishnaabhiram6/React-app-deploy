b4338db0fe752a433eb56d125ffe90acfd0a0cad
krishnaabhiram6 <krishnaabhiram666@gmail.com>
2025-12-27 02:02:05 +0530

    reactcodemodified

--- Samplereact/src/App.jsx
+++ Samplereact/src/App.jsx
@@ -1,8 +1,12 @@
-
+import Sample from "./Components/Sample"
 function App() {
   return (
     <>
     <h1>Deploying React Application</h1>
+    <Sample />
+    <Sample />
+    <Sample />
+    <Sample />
     </>
   )
 }
---
+++ Samplereact/src/Components/Sample.jsx
@@ -0,0 +1,8 @@
+export default function Sample(){
+    return(
+        <>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, consequuntur cum. Assumenda voluptatem enim, id sed odit laudantium libero vero necessitatibus officia ut ipsa cum vel at molestiae a reiciendis!</p>
+        <img src="purijagannadh.jpg" alt="" />
+        </>
+    )
+}
